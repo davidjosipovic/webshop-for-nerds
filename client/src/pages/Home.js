350ba1838e5b6fb80f7ef2f498288217bd25211a
David Josipović <davidjosipovic19@gmail.com>
2023-07-13 10:55:09 +0200

    home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,15 +3,18 @@ import React from 'react';
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold underline mb-4 text-gray-800">
+      <h1 className="text-4xl font-bold underline mb-6 text-gray-800">
         Welcome to Our Website!
       </h1>
-      <p className="text-lg text-gray-700">
+      <p className="text-lg text-gray-700 max-w-md text-center">
         Thank you for visiting our website. We provide a wide range of products and services to meet your needs. Browse through our offerings and feel free to contact us if you have any questions.
       </p>
-      <p className="mt-4 text-gray-700">
+      <p className="mt-6 text-lg text-gray-700 max-w-md text-center">
         Start exploring our website to find what you're looking for!
       </p>
+      <button className="mt-8 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-lg">
+        Explore Now
+      </button>
     </div>
   );
 }
