c5f761cd317a9ed64b4f852fd9ecdea5e161eacc
David Josipović <davidjosipovic19@gmail.com>
2023-07-21 12:46:52 +0200

    product

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -76,7 +76,10 @@ const ProductDetails = () => {
         <div className="ml-4">
           <p>{product.description}</p>
           <p className="text-lg font-bold mt-4">{product.price}€</p>
-          <button onClick={handleAddToCart} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4">
+          <button
+            onClick={handleAddToCart}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded-lg"
+          >
             Add to Cart
           </button>
         </div>
