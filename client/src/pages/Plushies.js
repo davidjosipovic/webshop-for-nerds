51bb27a36d02f27985006a81d4040dfa7b6ec398
David Josipović <davidjosipovic19@gmail.com>
2023-08-04 09:13:03 +0200

    .

--- client/src/pages/Plushies.js
+++ client/src/pages/Plushies.js
@@ -22,6 +22,7 @@ const Plushies = () => {
     fetchProducts();
   }, []);
 
+  
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Plushies</h1>
